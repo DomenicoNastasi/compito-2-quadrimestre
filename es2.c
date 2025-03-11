fa0a81f6b0d1e2b8b22cc361c9b9da7225cf15d6
DomenicoNastasi <domenico.nastasi@davincimilazzo.edu.it>
2025-03-11 13:29:45 +0100

    Add files via upload

---
+++ es2.c
@@ -0,0 +1,12 @@
+int main(void){
+        char stringa[10] =  "aeiou";
+        printf("%s\n", stringa);
+        inverti_capitalizza_stringa(stringa, 6);
+        printf("%s\n", stringa);
+        return 0;
+}
+
+void inverti_capitalizza_stringa(char *stringa, int);
+for()
+ 
+
